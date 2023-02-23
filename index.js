1702a461c1a2cf6b6879d4163feaed6ae27acc83
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-02-23 20:20:38 +0600

    add file for view

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const port = process.env.PORT || 5000;
 require('./config/db')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    //   res.send('Hello World!')
+    res.sendFile(__dirname + "/./views/index.html");
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
