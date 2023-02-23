18593d2d8a968e0d3169bba4afbadbabbcc1db61
Amirul Islam Hridoy <amirulislamhridoy30687@gmail.com>
2023-02-23 20:09:27 +0600

    mongodb is connected

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+mongoose.connect(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.0exa02b.mongodb.net/?retryWrites=true&w=majority`)
+.then(() => console.log('mongodb is connected'))
+.catch(error => console.log(error.message))
+console.log('aaa', process.env.DB_NAME)
